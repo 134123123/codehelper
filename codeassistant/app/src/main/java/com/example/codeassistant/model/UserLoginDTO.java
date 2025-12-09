1c6550bc41c5c86e437737b030b2ada72850695e
123 <2559028257@qq.com>
2025-12-09 22:34:16 +0800

    Delete codeassistant directory

--- codeassistant/app/src/main/java/com/example/codeassistant/model/UserLoginDTO.java
+++
@@ -1,8 +0,0 @@
-public class UserLoginDTO {
-    private String username;
-    private String password;
-    public UserLoginDTO(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-}
