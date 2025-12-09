7ba568ed997fefc5027d0fbeeac5714410f1dfbd
123 <2559028257@qq.com>
2025-12-09 21:39:41 +0800

    Delete codeassistant directory

--- codeassistant/app/src/main/java/com/example/codeassistant/util/PreferenceKey.java
+++
@@ -1,8 +0,0 @@
-package com.example.codeassistant.util;
-
-import androidx.datastore.preferences.core.Preferences;
-
-public interface PreferenceKey<T> {
-    String getName();
-    Class<T> getType();
-}
